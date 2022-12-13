c6d52a4d4ca4e2ddbef6b99ebd22a0869a70c479
ChoHyoungSeo <francesco.johs@gmail.com>
2022-12-13 15:14:01 +0900

    Update: nlp_sub.py_encoding, data_path

--- jooong2/nlp_sub.py
+++ jooong2/nlp_sub.py
@@ -4,7 +4,7 @@ import pandas as pd
 import os
 pd.set_option('display.max_columns', None)
 pd.set_option('display.max_rows', None)
-data = pd.read_csv('/content/reviews_rawdata_4.csv',names=("flag","site","review"))
+data = pd.read_csv('/content/reviews_rawdata.csv',names=("flag","site","review"))
 data['review2'] = [re.sub('[^A-Za-z0-9가-힣]', '', s) for s in data['review']]
 data1 = data['review2']
-data1.to_csv('love.csv',index=False,encoding='utf-8-sig')
+data1.to_csv('pre_input_data.csv',index=False,encoding='utf-8')
